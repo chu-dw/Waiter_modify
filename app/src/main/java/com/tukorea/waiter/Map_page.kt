43d7ebb53e82468fb6373ca43d7b5b5796976383
Chu_dw <ehddnjs1747@daum.net>
2022-09-15 21:21:31 +0900

    0915work

---
+++ app/src/main/java/com/tukorea/waiter/Map_page.kt
@@ -0,0 +1,24 @@
+package com.tukorea.waiter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tukorea.waiter.databinding.ActivityMapPageBinding
+import net.daum.mf.map.api.MapView
+
+
+class Map_page : AppCompatActivity() {
+    private lateinit var binding: ActivityMapPageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_page)
+
+        binding = ActivityMapPageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val mapView = MapView(this)
+        binding.clKakaoMapView.addView(mapView)
+
+    }
+}
